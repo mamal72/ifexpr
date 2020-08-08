790f0f7b09331e499d328b0dece77e1945e3d2a6
mamal72 <m4m4lj@gmail.com>
2020-08-08 20:55:15 +0430

    [ci skip] Updates

---
+++ assets/js/search.js
@@ -0,0 +1,3 @@
+var typedoc = typedoc || {};
+            typedoc.search = typedoc.search || {};
+            typedoc.search.data = {"kinds":{"64":"Function","65536":"Type literal","4194304":"Type alias"},"rows":[{"id":0,"kind":4194304,"name":"Callable","url":"globals.html#callable","classes":"tsd-kind-type-alias tsd-has-type-parameter tsd-is-not-exported"},{"id":1,"kind":65536,"name":"__type","url":"globals.html#callable.__type","classes":"tsd-kind-type-literal tsd-parent-kind-type-alias tsd-is-not-exported","parent":"Callable"},{"id":2,"kind":64,"name":"ifexpr","url":"globals.html#ifexpr","classes":"tsd-kind-function tsd-has-type-parameter"}]};
